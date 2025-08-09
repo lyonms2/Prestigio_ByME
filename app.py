f5728426ef2bfc0c481826959964ee70df15e9e5
lyonms2 <54992907+lyonms2@users.noreply.github.com>
2025-08-09 20:30:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ if not st.session_state.logado:
     st.stop()
 
 # Se estiver logado, exibe o conteúdo do aplicativo
-st.title("📊 Monitor de Criptomoedas")
+st.title("📊 Monitor de Criptomoedasss")
 #st.set_page_config(page_title="Monitor de Criptomoedas", layout="wide")
 
 # ======================
@@ -288,6 +288,7 @@ if st.session_state.df_restantes is not None:
         st.dataframe(df_filtrado_restantes, use_container_width=True)
     else:
         st.dataframe(st.session_state.df_restantes, use_container_width=True)
+
 
 
 
